52a3acb30651ccfc30497dab170d5fc1aecf2246
aarif <mraarif@outlook.com>
2019-11-11 16:03:17 +0500

    added the support for django2.2
    updated the django version
    
    updated the django version
    
    updated the travis config as suggested
    
    changes made as suggested

--- help_tokens/__init__.py
+++ help_tokens/__init__.py
@@ -6,6 +6,6 @@ from __future__ import absolute_import, unicode_literals
 
 from .context_processor import context_processor
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 default_app_config = 'help_tokens.apps.HelpTokensConfig'  # pylint: disable=invalid-name

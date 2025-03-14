b2efdfa558057f83367499802c13ed4e695d95cc
sarina <sarina@axim.org>
2025-03-14 11:13:24 -0400

    feat!: Make help-tokens work with docs.openedx.org
    We've migrated the docs to docs.openedx.org. Reconstruct URLs
    to work with docs.openedx.org URLs.

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,5 +9,5 @@ Tests for the help-tokens urls and views.
 def test_redirect(client, book_settings):
     response = client.get("/instructor")
     assert response.status_code == 302
-    expected = "http://edx.readthedocs.io/projects/learner-guide/en/ver/SFD_instructor_dash_help.html"
+    expected = "https://docs.openedx.org/en/latest/learners/SFD_instructor_dash_help.html"
     assert response['location'] == expected
